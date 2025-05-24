1ded99b18732c6e690cfcb4a3cd3c36fef15261c
Shaun Jacobsen <shaun@sjacobsen.com>
2025-05-24 18:26:41 +0200

    update webhook processing

--- app/index.ts
+++ app/index.ts
@@ -1,14 +1,53 @@
 import express, { Request, Response } from 'express';
-const { Client, CheckoutAPI, Webhook } = require('@adyen/api-library');
+const {
+  Client,
+  CheckoutAPI,
+  Webhook,
+  hmacValidator,
+} = require('@adyen/api-library');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const adyenClient = new Client({
+  apiKey: process.env.adyen_api_key,
+  environment: 'TEST',
+});
 
 app.use(express.json());
 
 app.post('/webhook', (req: Request, res: Response) => {
   console.log('Webhook received:', req.body);
-  res.status(200).send('Received');
+
+  // this code was taken from https://github.com/adyen-examples/adyen-node-online-payments/tree/main/checkout-example
+  const hmacKey = process.env.ADYEN_HMAC_KEY;
+  const validator = new hmacValidator();
+
+  const notificationRequest = req.body;
+  const notificationRequestItems = notificationRequest.notificationItems;
+
+  // fetch first (and only) NotificationRequestItem
+  const notification = notificationRequestItems[0].NotificationRequestItem;
+  console.log(notification);
+
+  // Handle the notification
+  if (validator.validateHMAC(notification, hmacKey)) {
+    // valid hmac: process event
+    const merchantReference = notification.merchantReference;
+    const eventCode = notification.eventCode;
+    console.log(
+      'merchantReference:' + merchantReference + ' eventCode:' + eventCode,
+    );
+
+    // do something like update db, this is up to the client
+
+    // acknowledge event has been consumed
+    res.status(202).send(); // Send a 202 response with an empty body
+  } else {
+    // invalid hmac
+    console.log('Invalid HMAC signature: ' + notification);
+    res.status(401).send('Invalid HMAC signature');
+  }
 });
 
 app.get('/', (req: Request, res: Response) => {
